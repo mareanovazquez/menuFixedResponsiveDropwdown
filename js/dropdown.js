d52f39adedd299653c83e8f7d942f8f032155c30
Mariano Vázquez <mareanovazquez@protonmail.com>
2024-04-30 09:15:24 -0300

    add styles and function to dropdown

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,15 +1,31 @@
-const primera = document.getElementById("primera");
+const inicioDropdown = document.getElementById("inicio-dropdown-desktop");
 const menuDesplegado = document.getElementById("menuDesplegado");
 const inicioMenuCelu = document.getElementById('inicioMenuCelu');
 const subMenuCelu = document.getElementById('menuCeluDesplegado');
+const seleccionInicio = document.querySelectorAll('.seleccion');
 
-function deployMenuPrimera() {
+/* FUNCIONES DROPDOWN INICIO DESKTOP */
+function deployDropdownInicio() {
     menuDesplegado.classList.add("menuDesplegado");
 }
-
-function retraerMenuPrimera() {
+function retractDropdownInicio() {
     menuDesplegado.classList.remove('menuDesplegado');
 }
+
+inicioDropdown.addEventListener("mouseover", deployDropdownInicio);
+menuDesplegado.addEventListener("mouseleave", retractDropdownInicio);
+
+seleccionInicio.forEach(function(item) {
+    item.addEventListener('click', retractDropdownInicio);
+});
+
+window.addEventListener('keydown', (event) => {
+    if (event.code === 'Escape') {
+        menuDesplegado.classList.remove('menuDesplegado');
+    }
+});
+
+/* FUNCIONES DROPDOWN INICIO MOBILE */
 
 function deploySubMenuInicio() {
     subMenuCelu.classList.toggle('subMenuCeluDesplegado');
@@ -36,16 +52,16 @@ function cerrarMenu(event) {
     }
 }
 
-primera.addEventListener("mouseover", deployMenuPrimera);
-menuDesplegado.addEventListener("mouseleave", retraerMenuPrimera);
+
 inicioMenuCelu.addEventListener("mouseover", deploySubMenuInicio);
 
 // Agrega evento click a los elementos del submenú
 const itemsSubmenu = document.querySelectorAll('.itemSubmenuCelu');
 itemsSubmenu.forEach(item => {
-    item.addEventListener('click', function() {
+    item.addEventListener('click', function () {
         cerrarMenu({ key: 'Escape' }); // Llama a cerrarMenu al hacer clic en un elemento del submenú
     });
 });
 
 window.addEventListener("keydown", cerrarMenu);
+
